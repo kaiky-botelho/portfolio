5c54e1c61cde4d4f759bce81863534f5cb1cb975
Kaiky-Botelho <kaii.bots18@gmail.com>
2025-02-26 12:56:54 -0300

    Começo_Introducao

--- src/app/introducao.tsx
+++ src/app/introducao.tsx
@@ -1,10 +1,15 @@
-import { BackgroundBeams } from "@/components/ui/background-beams";
-import "../styles/global.css";
+import "../styles/introducao/introducao.css";
 
 export default function Introducao() {
   return (
-    <main className="introducao ">
-      
+    <main className="introducao container">
+      <div className="introducao-conteudo">
+        <img src="../assets/Polygon 1.svg" alt="Descrição da imagem" />
+        <p>Olá, me chamo Kaiky e sou um</p>
+        <h1>
+          DESENVOLVEDOR <span>FRONT-END</span> <br /> & <span>UI/UX DESIGNER</span>
+        </h1>
+      </div>
     </main>
   );
 }
