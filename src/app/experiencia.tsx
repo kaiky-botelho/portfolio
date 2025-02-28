108ec5d2ae575e092c04b1cd0b5869f387fd7294
kaiky botelho <kaii.bots18@gmail.com>
2025-02-28 16:44:46 -0300

    EXPERIENCIA

--- src/app/experiencia.tsx
+++ src/app/experiencia.tsx
@@ -8,8 +8,9 @@ export default function Experiencia() {
   <div className="experiencia-itens">
     <div className="experiencia-item">
     <Timeline>
-      <TimelineItem timestamp="2024 - ATUAL" type="Técnico" subtitle="DESENVOLVIMENTO DE SISTEMAS" title="Senai Marcio Bagueira Leal" />
-      <TimelineItem timestamp="2023 - 2024" type="Técnico" subtitle="DESENVOLVIMENTO DE SISTEMAS" title="Etec Pedro Badran" />
+      <TimelineItem timestamp="2024 - ATUAL" type="Técnico" subtitle="DESENVOLVIMENTO DE SISTEMAS" title="SENAI Marcio Bagueira Leal" />
+      <TimelineItem timestamp="2023 - 2024" type="Técnico" subtitle="DESENVOLVIMENTO DE SISTEMAS" title="ETEC Pedro Badran" />
+      <TimelineItem timestamp="2020 - 2022"  subtitle="Ensino Médio" title="ETEC Professor José Ignácio Azevedo Filho" />
     </Timeline>
     </div>
     <div className="experiencia-item">
